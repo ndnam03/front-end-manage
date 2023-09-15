802dab96dd79c6c0f4c0dbab9eb7ae1872fbb1ac
ndnam03 <nguyendinhnam1030@gmail.com>
2023-09-15 09:39:42 +0700

    upload project

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import Vuex from "vuex";
+
+const store = new Vuex.Store({
+  // Các state, mutations, actions, getters...
+});
+
+export default store;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <header-view />
+    <SidebarView />
+  </div>
+</template>
+
+<script setup>
+// @ is an alias to /src
+import HeaderView from "@/components/HeaderView.vue";
+import SidebarView from "@/components/SideBarView.vue";
+</script>
